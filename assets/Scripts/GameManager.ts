9a14e7ae843570f71ec5737c710afa95a40a29cc
huynhthuan <huynhthuanvugia@gmail.com>
2024-08-19 20:34:16 +0700

    add ui,controller

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,162 @@
+import { _decorator, CCInteger, Component, director, instantiate, Label, Node, Prefab, Vec3 } from 'cc';
+import { BLOCK_SIZE, PlayerController } from './PlayerController';
+
+const { ccclass, property } = _decorator;
+
+enum BlockType {
+    BT_NONE,
+    BT_STONE,
+};
+
+enum GameState {
+    GS_INIT,
+    GS_PLAYING,
+    GS_END,
+};
+
+@ccclass('GameManager')
+export class GameManager extends Component {
+
+    @property({ type: Prefab })
+    public boxPrefab: Prefab | null = null;
+
+    @property({ type: Node })
+    public canvas: Node | null = null;
+
+    @property({ type: CCInteger })
+    public roadLength: number = 5;
+    public _road: BlockType[] = [];
+
+    @property({ type: Node })
+    public startMenu: Node | null = null;
+
+    @property({ type: PlayerController })
+    public playerCtrl: PlayerController | null = null;
+
+    @property({ type: Label })
+    public stepsLabel: Label | null = null;
+
+    start() {
+        this.setCurState(GameState.GS_INIT);
+        this.playerCtrl?.node.on('JumpEnd', this.onPlayerJumpEnd, this);
+    }
+
+    update(deltaTime: number) {
+
+    }
+
+    generateRoad() {
+        console.log("generateRoad");
+
+        this.node.removeAllChildren();
+
+        this._road = [];
+        // startPos
+        this._road.push(BlockType.BT_STONE);
+
+        for (let i = 1; i < this.roadLength; i++) {
+            if (this._road[i - 1] === BlockType.BT_NONE) {
+                this._road.push(BlockType.BT_STONE);
+            } else {
+                this._road.push(Math.floor(Math.random() * 2));
+            }
+        }
+
+        for (let j = 0; j < this._road.length; j++) {
+            let block: Node | null = this.spawnBlockByType(this._road[j]);
+
+
+            if (block) {
+                // this.node.addChild(block);
+                this.canvas.addChild(block);
+
+                block.setPosition(j * BLOCK_SIZE, 0, 0);
+            }
+        }
+    }
+
+    spawnBlockByType(type: BlockType) {
+        if (!this.boxPrefab) {
+            return null;
+        }
+
+        let block: Node | null = null;
+        switch (type) {
+            case BlockType.BT_STONE:
+                block = instantiate(this.boxPrefab);
+                break;
+        }
+
+        return block;
+    }
+
+    setCurState(value: GameState) {
+        switch (value) {
+            case GameState.GS_INIT:
+                this.init();
+                break;
+            case GameState.GS_PLAYING:
+                this.playing();
+                break;
+            case GameState.GS_END:
+                break;
+        }
+    }
+
+    playing() {
+        if (this.startMenu) {
+            this.startMenu.active = false;
+        }
+
+        if (this.stepsLabel) {
+            this.stepsLabel.string = '0';
+        }
+
+        setTimeout(() => {
+            if (this.playerCtrl) {
+                this.playerCtrl.setInputActive(true);
+            }
+        }, 0.1);
+    }
+
+    init() {
+        if (this.startMenu) {
+            this.startMenu.active = true;
+        }
+
+        this.generateRoad();
+
+
+        if (this.playerCtrl) {
+
+            this.playerCtrl.setInputActive(false);
+
+            this.playerCtrl.node.setPosition(Vec3.ZERO);
+            this.playerCtrl.reset();
+        }
+    }
+
+    onStartButtonClicked() {
+        this.setCurState(GameState.GS_PLAYING);
+    }
+
+    onPlayerJumpEnd(moveIndex: number) {
+        //update steps label.
+        if (this.stepsLabel) {
+            this.stepsLabel.string = '' + (moveIndex >= this.roadLength ? this.roadLength : moveIndex);
+        }
+        this.checkResult(moveIndex);
+    }
+
+    checkResult(moveIndex: number) {
+        if (moveIndex < this.roadLength) {
+            if (this._road[moveIndex] == BlockType.BT_NONE) {   //steps on empty block, reset to init.
+                this.setCurState(GameState.GS_INIT);
+            }
+        } else {    //out of map, reset to init.
+            this.setCurState(GameState.GS_INIT);
+        }
+    }
+}
+
+
---
+++ assets/Scripts/PlayerController.ts
@@ -0,0 +1,128 @@
+import { _decorator, Animation, Component, EventMouse, Input, input, Vec3 } from 'cc';
+
+const { ccclass, property } = _decorator;
+export const BLOCK_SIZE = 40;
+
+@ccclass('PlayerController')
+export class PlayerController extends Component {
+
+
+    @property(Animation)
+    BodyAnim: Animation = null;
+
+    private _startJump: boolean = false;
+    private _jumpStep: number = 0;
+    private _jumpTime: number = 0.1;
+    private _curJumpTime: number = 0;
+    private _curJumpSpeed: number = 0;
+    private _curPos: Vec3 = new Vec3();
+    private _deltaPos: Vec3 = new Vec3(0, 0, 0);
+    private _targetPos: Vec3 = new Vec3();
+    private _curMoveIndex: number = 0;
+
+    start() {
+        // console.clear();
+        // console.log("start");
+        // input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+    }
+
+    update(deltaTime: number) {
+        if (!this._startJump) {
+            return;
+        }
+
+        this._curJumpTime += deltaTime;
+        if (this._curJumpTime > this._jumpTime) {
+            this.node.setPosition(this._targetPos);
+            this._startJump = false;
+            this.onOnceJumpEnd();
+        } else {
+            this.node.getPosition(this._curPos);
+            this._deltaPos.x = this._curJumpSpeed * deltaTime;
+            Vec3.add(this._curPos, this._curPos, this._deltaPos);
+            this.node.setPosition(this._curPos);
+        }
+    }
+
+    onMouseUp(event: EventMouse) {
+        if (event.getButton() === EventMouse.BUTTON_LEFT) {
+            this.jumpByStep(1);
+        } else if (event.getButton() === EventMouse.BUTTON_RIGHT) {
+            this.jumpByStep(2);
+        }
+    }
+
+    jumpByStep(step: number) {
+        if (this._startJump) {
+            return;
+        }
+        
+        const clipName = step == 1 ? 'oneStep' : 'twoStep';
+        const state = this.BodyAnim.getState(clipName);
+        this._jumpTime = state.duration;
+
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+        this._curJumpSpeed = this._jumpStep * BLOCK_SIZE / this._jumpTime;
+        this.node.getPosition(this._curPos);
+        Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep * BLOCK_SIZE, 0, 0));
+
+        if (this.BodyAnim) {
+            if (step === 1) {
+                this.BodyAnim.play('oneStep');
+            } else if (step === 2) {
+                this.BodyAnim.play('twoStep');
+            }
+        }
+
+        this._curMoveIndex += step;
+    }
+
+    onOnceJumpEnd() {
+        this.node.emit('JumpEnd', this._curMoveIndex);
+    }
+
+    // jumpByStep(step: number) {
+
+    //     if (this._startJump) {
+    //         return;
+    //     }
+
+    //     this._startJump = true;
+    //     const clipName = step == 1 ? 'oneStep' : 'twoStep';
+    //     const state = this.BodyAnim.getState(clipName);
+    //     this._jumpTime = state.duration;
+
+    //     this._jumpStep = step;
+
+    //     this._curJumpTime = 0;
+    //     this._curJumpSpeed = this._jumpStep * BLOCK_SIZE / this._jumpTime;
+    //     this.node.getPosition(this._curPos);
+    //     Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep * BLOCK_SIZE, 0, 0));
+
+    //     if (this.BodyAnim) {
+    //         if (step === 1) {
+    //             this.BodyAnim.play('oneStep');
+    //         } else if (step === 2) {
+    //             this.BodyAnim.play('twoStep');
+    //         }
+    //     }
+    // }
+
+    setInputActive(active: boolean) {
+        if (active) {
+            input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        } else {
+            input.off(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        }
+    }
+
+    reset() {
+        this._curMoveIndex = 0;
+        this.node.getPosition(this._curPos);
+        this._targetPos.set(0, 0, 0);
+    }
+}
+
+
